1d874a18682aec89a4767a796618a833d31de58f
shettysatvikj <shettysatvik@gmail.com>
2026-02-17 21:10:14 +0530

    Fix CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ import { connectDB } from "./config/db.js";
 const app = express();
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://doc-ease-1.onrender.com"
-  ],
+  origin: true,
   credentials: true
 }));
 
