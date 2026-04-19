27d5e99dbe2e8356222c37f68a685131583394b0
Shetty Satvik Jayram Lata <shettysatvikj@gmail.com>
2026-04-19 18:38:34 +0530

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: "https://dr-ease.onrender.com", // backend URL
 });
 
 // ✅ Automatically attach JWT token
